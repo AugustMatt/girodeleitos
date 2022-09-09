a5aa9638eef0bf3b95b3e37c603baf54eb45300b
Matheus Augusto <matheusaugusto@NATALCENTER.LOCAL>
2022-09-09 13:15:57 -0300

    :racehorse: Primeira versão do processo de login quase terminada.

--- src/Login.vue
+++ src/Login.vue
@@ -6,16 +6,6 @@
 
 </script>
 
-<script>
-    addEventListener("load", function () {
-      setTimeout(hideURLbar, 0);
-    }, false);
-
-    function hideURLbar() {
-      window.scrollTo(0, 1);
-    }
-</script>
-
 <template>
 
     <LoadingScreen />
--- src/components/login/Content.vue
+++ src/components/login/Content.vue
@@ -1,16 +1,72 @@
+<script>
+import { onErrorCaptured } from 'vue';
+
+    export default{
+        email: 'AddInformation',
+        data(){
+            return{
+                email: '',
+                senha: '',
+                remember: false,
+            }
+        },
+        methods:{
+            onSubmit(e){
+
+                e.preventDefault();
+
+                if(!this.email.trim() || this.email.length < 5 || !this.email.includes('@')){
+                    alert('Digite um email valido');
+                    return;
+                } 
+
+                var myHeaders = new Headers();
+
+                var myInit = { method: 'POST',
+                            headers: myHeaders,
+                            mode: 'cors',
+                            cache: 'default',
+                            body: new FormData(document.getElementById('formLogin'))
+                         };
+
+                fetch('http://127.0.0.1:8000/api/users/sexo', myInit)
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(this.handleErrors)
+                .then(function(data) {
+
+                    alert('ok');
+                
+                }).catch(function(error) {
+                    alert(error.message);
+                });
+
+            },
+            handleErrors(response){
+                if(response.error != undefined){
+                    throw Error(response.error);
+                }
+                return response;
+            }
+        }
+    }
+
+</script>
+
 <template>
     <div class="header-left-bottom">
-        <form action="#" method="post">
+        <form id="formLogin" @submit="onSubmit" method="post">
             <div class="icon1">
                 <span class="fa fa-user"></span>
-                <input type="email" placeholder="Email" required />
+                <input name="email" v-model="email" type="email" placeholder="Email" required />
             </div>
             <div class="icon1">
                 <span class="fa fa-lock"></span>
-                <input type="password" placeholder="Senha" required />
+                <input name="senha" v-model="senha" type="password" placeholder="Senha" required />
             </div>
             <div class="login-check">
-                <label class="checkbox"><input type="checkbox" name="checkbox" checked><i> </i> Mantenha-me Logado</label>
+                <label class="checkbox"><input v-model="remember" type="checkbox" name="checkbox"><i> </i> Mantenha-me Logado</label>
             </div>
             <div class="bottom">
                 <button class="btn">Log In</button>
